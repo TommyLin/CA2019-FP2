09fdee3b10793789be17badf1497715da23aaec7
林宏文(Tommy Lin) <tommy.lin.1101@gmail.com>
2019-12-25 00:22:58 +0800

    Bug fix

--- FMIndex2.cu
+++ FMIndex2.cu
@@ -5,10 +5,9 @@
 #include <cmath>
 #include <stdlib.h>
 #include<sys/time.h>
-#define BLOCKS 1
-#define THREADS 256
+#define MAX 10000000
 using namespace std;
-
+int **L_counts;
 void run_sort(void);
 int compSuffixes(char *suffix1, char *suffix2, int length);
 
@@ -21,170 +20,185 @@ int **SA_Final_student;
 int **L_counts_student;
 char *L_student;
 int F_counts_student[]={0,0,0,0};
-int num_value=0;
+int num_value = 0;
 int read_count = 0;
 int read_length = 0;
+int BLOCKS, THREADS;
 
-__global__ void bitonic_sort_step(char *dev_values, char* result, int j, int k, int num_value, int read_length){
+__global__ void bitonic_sort_step(char *dev_values, int j, int k, int num_value, int read_length, int read_count, int *dev_SA_Final_1, int *dev_SA_Final_2){
     //printf("gfdgfdgdsfg\n");
     int flag = 0;
 	unsigned int i, ixj; /* Sorting partners: i and ixj */
     i = threadIdx.x + blockDim.x * blockIdx.x;
     ixj = i^j;
     
-    //printf("enter GPU2\n");
-    //printf("sizeof_values = %d\n",  );
-    //printf("original string=");
-    //for(int k=0;k<num_value*num_value;k++)
-        //printf("%c", dev_values[k]);
-    //if(i==1) printf("original string:%s\n", dev_values[0]);
-    //if(i==2)
-        //for(int kk=0; kk<num_value*read_length; k = k+1) 
-            //printf("%c", dev_values[kk]);
-    //printf("original string=");
-    /*for(int kk=0;kk<num_value;kk++){
-        
-        //dev_values[(kk+1)*read_length-1]='\0';
-        if((kk==0 || kk==1||kk==2||kk==3||kk==4)&& i==1){
-            printf("%d\tdev_values=%s\n",k,&dev_values[kk*read_length]);
-            //printf("========== i=%d ==========\n",k);
-        }
-    }*/
+
     /* The threads with the lowest ids sort the array. */
     flag = 0;
-    int fir, sec;
-    fir = 0;
-    sec = 32;
-    //printf("%d and %d\n", i, ixj);
-    if(i==0)printf("%c and %c\n", dev_values[fir*65], dev_values[sec*65]);
-    for(int l=0;l<read_length;l++){
-        //if(i*read_length+l==0 && ixj*read_length+l==64*65)printf("%d, %d, %c and %c\n", i*read_length+l, ixj*read_length+l, dev_values[i*read_length+l], dev_values[ixj*read_length+l]);
-        if(dev_values[i*read_length+l]>dev_values[ixj*read_length+l]){
-            if(i==0&&i*read_length+l==fir*65 && ixj*read_length+l==sec*65)printf(">>>>>>>>>>>>>>>>>>\n");
-            flag = 1;
-            break;
-        }
-        else if(dev_values[i*read_length+l]<dev_values[ixj*read_length+l]){
-            if(i==0&&i*read_length+l==fir*65 && ixj*read_length+l==sec*65)printf("<<<<<<<<<<<<<<<<<<<<\n");
-            flag = -1;
-            break;
-        }
-        if(i==0&&i*read_length+l==fir*65 && ixj*read_length+l==sec*65)printf("=========================\n");
-        flag = 0;
-    }
+
     if ((ixj)>i) {
+        for(int l=0;l<read_length;l++){
+            if(dev_values[i*read_length+l]>dev_values[ixj*read_length+l]){
+                //if(i==0&&i*read_length+l==fir*65 && ixj*read_length+l==sec*65)printf(">>>>>>>>>>>>>>>>>>\n");
+                flag = 1;
+                break;
+            }
+            else if(dev_values[i*read_length+l]<dev_values[ixj*read_length+l]){
+                //if(i==0&&i*read_length+l==fir*65 && ixj*read_length+l==sec*65)printf("<<<<<<<<<<<<<<<<<<<<\n");
+                flag = -1;
+                break;
+            }
+            //if(i==0&&i*read_length+l==fir*65 && ixj*read_length+l==sec*65)printf("=========================\n");
+            flag = 0;
+            
+        }
+        //printf("i=%d, ixj=%d, sorting result flag = %d\n",i,ixj,flag);
+        
+
         if ((i&k)==0) {
             // Sort ascending //
             //printf("1110");
             //for(int m=0;m<num_value;m++){
 
                 if (flag==1) {
-                    //printf("2222");
-                    // exchange(i,ixj); //      
-                    char* temp = new char[read_length];
-                    for(int kk=0;kk<read_length;kk++){
-                        temp[kk] = dev_values[i*read_length+kk];
-                        dev_values[i*read_length+kk] = dev_values[ixj*read_length+kk];
-                        dev_values[ixj*read_length+kk] = temp[kk];
-                    }
-                    //break;
+                    //printf("3333, %d, %d\n", i, ixj);  
+                    char* temp;
+					temp = (char*)malloc(sizeof(char)*read_length);
+					memcpy(temp, &dev_values[i*read_length], read_length*sizeof(char));
+					memcpy(&dev_values[i*read_length], &dev_values[ixj*read_length], read_length*sizeof(char));
+					memcpy(&dev_values[ixj*read_length], temp, read_length*sizeof(char));
+                    int temp_SA_1;
+                    temp_SA_1 = dev_SA_Final_1[i];
+                    dev_SA_Final_1[i] = dev_SA_Final_1[ixj];
+                    dev_SA_Final_1[ixj] = temp_SA_1;
+                    int temp_SA_2;
+                    temp_SA_2 = dev_SA_Final_2[i];
+                    dev_SA_Final_2[i] = dev_SA_Final_2[ixj];
+                    dev_SA_Final_2[ixj] = temp_SA_2;
+					free(temp);
                 }
-            //} 
         }
         if ((i&k)!=0) {
             // Sort descending 
-            for(int m=0;m<num_value;m++){
-                if (flag==-1) {
-                    //printf("2222");
-                    // exchange(i,ixj);         
-                    char* temp = new char[read_length];
-                    for(int kk=0;kk<read_length;kk++){
-                        temp[kk] = dev_values[i*read_length+kk];
-                        dev_values[i*read_length+kk] = dev_values[ixj*read_length+kk];
-                        dev_values[ixj*read_length+kk] = temp[kk];
-                    }
-                }
+            
+            if (flag==-1) {
+                //printf("2222, %d, %d\n", i, ixj);         
+                char* temp;
+                temp = (char*)malloc(sizeof(char)*read_length);
+                memcpy(temp, &dev_values[i*read_length], read_length*sizeof(char));
+                memcpy(&dev_values[i*read_length], &dev_values[ixj*read_length], read_length*sizeof(char));
+                memcpy(&dev_values[ixj*read_length], temp, read_length*sizeof(char));
+                int temp_SA_1;
+                temp_SA_1 = dev_SA_Final_1[i];
+                dev_SA_Final_1[i] = dev_SA_Final_1[ixj];
+                dev_SA_Final_1[ixj] = temp_SA_1;
+                int temp_SA_2;
+                temp_SA_2 = dev_SA_Final_2[i];
+                dev_SA_Final_2[i] = dev_SA_Final_2[ixj];
+                dev_SA_Final_2[ixj] = temp_SA_2;
+				free(temp);
             }
         }
     }
 }
-void bitonic_sort(char **values, int read_length){
+void bitonic_sort(char **values){
     char *dev_values;
-    size_t size = num_value * sizeof(char);
+    size_t size = read_length * sizeof(char);
     char *temp;
-    char* temp_char = new char[read_length];
-    char* result;
-    temp = (char*)malloc(read_length*size);
-    cout<<"read_length="<<read_length<<endl;
+    char *temp_char = new char[read_length];
+    int* SA_Final_1 = new int[num_value];
+    int* SA_Final_2 = new int[num_value];
+    int* dev_SA_Final_1;
+    int* dev_SA_Final_2;
+    temp = (char*)malloc(num_value*size);
     for(int i=0;i<read_length;i++){
         temp_char[i]='T';
     }
-    cout<<"num_value="<<num_value<<endl;
     for(int i=0;i<num_value;i++){
-        if(i<read_length)
+        if(i<read_length*read_count){
             memcpy(&temp[i*read_length],values[i],read_length*sizeof(char));
+            SA_Final_1[i] = SA_Final_student[i][0];
+            SA_Final_2[i] = SA_Final_student[i][1];
+        }
         else{
-            cout<<"into if "<<endl;
             memcpy(&temp[i*read_length],temp_char,read_length*sizeof(char));   
         }
     }
-    cout<<"==============================="<<endl;
-    cout<<"temp_char="<<temp_char<<endl;
-    cout<<"==============================="<<endl;
-    cout<<"temp="<<temp<<endl;
-    cout<<"==============================="<<endl;
-	for(int i=0;i<read_length;i++)
-        cout<<"values="<<values[i]<<endl;
-	cout<<"========================="<<endl;
-    fprintf(stderr,"enter bitonic sort\n");
-    cudaMalloc((void**) &dev_values, size*read_length);
-    cout<<"dfsfsdfsdf"<<size<<num_value<<endl;
-    cudaMalloc((void**) &result, size*num_value);
+    cudaMalloc((void**) &dev_values, size*num_value);
+    cudaMalloc((void**) &dev_SA_Final_1, num_value*sizeof(int));
+    cudaMalloc((void**) &dev_SA_Final_2, num_value*sizeof(int));
     
     
-    cudaMemcpy(dev_values, temp, read_length*size, cudaMemcpyHostToDevice);
-    fprintf(stderr,"finish pushing data into device\n");
+    cudaMemcpy(dev_values, temp, num_value*size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_SA_Final_1, SA_Final_1, num_value*sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_SA_Final_2, SA_Final_2, num_value*sizeof(int), cudaMemcpyHostToDevice);
 
     dim3 blocks(BLOCKS,1);    /* Number of blocks   */
     dim3 threads(THREADS,1);  /* Number of threads  */
 
     int j, k;
     /* Major step */
+    
     for (k = 2; k <= num_value; k <<= 1) {
         //* Minor step */
         for (j=k>>1; j>0; j=j>>1) {
-			fprintf(stderr,"before entering GPU\n");
-			bitonic_sort_step<<<1, 1>>>(dev_values,result, j, k, num_value,read_length);
-			fprintf(stderr,"leaving GPU\n");
-        }
+			bitonic_sort_step<<<blocks, threads>>>(dev_values, j, k, num_value,read_length, read_count ,dev_SA_Final_1,dev_SA_Final_2);
+		}
     }
-   
+      
 		
     //for(int i=0;i<read_length;i++){
         //cudaMemcpy(values[i], &dev_values[i*read_length], read_length*sizeof(char), cudaMemcpyDeviceToHost);       
     //}
-    cudaMemcpy(temp, dev_values, read_length*size, cudaMemcpyDeviceToHost);
-    
-    cout<<"begin teeeeeeeeeeeeeeeeeeeeeeeeeeeeeemp"<<endl;
-    for(int i=0;i<read_length;i++){
+    cudaMemcpy(temp, dev_values, read_length*read_count*size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(SA_Final_1, dev_SA_Final_1, num_value*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(SA_Final_2, dev_SA_Final_2, num_value*sizeof(int), cudaMemcpyDeviceToHost);
+   
+    //cout<<"begin teeeeeeeeeeeeeeeeeeeeeeeeeeeeeemp"<<endl;
+    for(int i=0;i<read_length*read_count;i++){
+
         memcpy(values[i],&temp[i*read_length],read_length*sizeof(char));
-        cout<<values[i]<<endl;
+        SA_Final_student[i][0] = SA_Final_1[i];
+        SA_Final_student[i][1] = SA_Final_2[i];
+        /*if(i==0){
+              for(int i=0; i<read_count*read_length;i++){
+                for(int j=0;j<4;j++)
+                        //cout<<"original answer for"<<i<<","<<j<<" is "<<L_counts[i][j]<<endl; 
+                          printf("original answer for%d,%d is %d\n",i, j, L_counts[i][j]);
+                
+            }  
+        } */
+        //cudaMemcpy(SA_Final_student[i][0], dev_SA_Final_1, read_length*size, cudaMemcpyDeviceToHost);
+        //cout<<values[i]<<endl;
     }  
-    
-    cout<<"tttttttttttttttteeeeeeeeeeemmmmmmmmmmmmmmmpppppppppppppppppppppp"<<endl;
-    cout<<temp<<endl;
+      
+    //cout<<"tttttttttttttttteeeeeeeeeeemmmmmmmmmmmmmmmpppppppppppppppppppppp"<<endl;
+    //cout<<temp<<endl;
     //for(int i=0;i<read_length;i++)
         //cout<<"i="<<i<<"values=\t\t"<<values[i]<<endl;
+	free(temp);
     cudaFree(dev_values);
+	cudaFree(SA_Final_1);
+	cudaFree(SA_Final_2);
 }
 
 
 //Calculates the final FM-Index
 int** makeFMIndex_student(char ***suffixes, int read_count, int read_length, int F_count[], char *L_student){
+
+     
     int i, j;
-    int temp_stu = ceil(log2((float)read_length));
+    int temp_stu = ceil(log2((float)read_length*read_count));
 	num_value = pow(2,temp_stu);
-    cout<<"======================="<<endl;
+	if(num_value<=256){
+		THREADS = num_value;
+		BLOCKS = 1;
+	}
+	else{
+		THREADS = 256;
+		BLOCKS = num_value/THREADS;
+	}
+    /*cout<<"======================="<<endl;
     cout<<"read_length = "<<read_length<<endl;
     cout<<"======================="<<endl;
     cout<<"======================="<<endl;
@@ -192,13 +206,16 @@ int** makeFMIndex_student(char ***suffixes, int read_count, int read_length, int
     cout<<"======================="<<endl;
     cout<<"======================="<<endl;
     cout<<"num_value = "<<num_value<<endl;
-    cout<<"======================="<<endl;
+    cout<<"======================="<<endl;*/
     
     SA_Final_student=(int**)malloc(read_count*read_length*sizeof(int*));
+
+    
     for(i=0;i<read_count*read_length;i++){
         
         SA_Final_student[i]=(int*)malloc(2*sizeof(int));
     }
+
     //Temporary storage for collecting together all suffixes
     char **temp_suffixes=(char**)malloc(read_count*read_length*sizeof(char*));
 
@@ -211,44 +228,36 @@ int** makeFMIndex_student(char ***suffixes, int read_count, int read_length, int
             SA_Final_student[i*read_length+j][1]=i;
         }
     }
+    /*for(int i=0;i<read_count;i++){
+        for(int j=0;j<read_length;j++)
+            cout<<temp_suffixes[i*read_length+j]<<endl;
+    }*/
+    cout<<endl;
     cout<<"read_count = "<<read_count<<endl;
     cout<<"read_length = "<<read_length<<endl;
     char *temp=(char*)malloc(read_length*sizeof(char));
     
     int **L_count=(int**)malloc(read_length*read_count*sizeof(int*));
-    for(i=0;i<read_length*read_count;i++){
-        L_count[i]=(int*)malloc(4*sizeof(int));
-        for(j=0;j<4;j++){
+    for(int i=0;i<read_length*read_count;i++){
+        //L_count[i]=(int*)malloc(4*sizeof(int));
+		L_count[i]=new int[4];
+        for(int j=0;j<4;j++){
             L_count[i][j]=0;
         }
     }
 
-    //run_sort();
-    //Focus on improving this for evaluation purpose
-    //Sorting of suffixes
-    /*for(i=0;i<read_count*read_length-1;i++){
-        for(j=0;j<read_count*read_length-i-1;j++){
-            if(compSuffixes(temp_suffixes[j], temp_suffixes[j+1], read_length)>0){
-                memcpy(temp, temp_suffixes[j], read_length*sizeof(char));
-                memcpy(temp_suffixes[j], temp_suffixes[j+1], read_length*sizeof(char));
-                memcpy(temp_suffixes[j+1], temp, read_length*sizeof(char));
-                int temp_int = SA_Final_student[j][0];
-                SA_Final_student[j][0]=SA_Final_student[j+1][0];
-                SA_Final_student[j+1][0]=temp_int;
-                temp_int = SA_Final_student[j][1];
-                SA_Final_student[j][1]=SA_Final_student[j+1][1];
-                SA_Final_student[j+1][1]=temp_int;
-            }
-        }
-    }*/
-	bitonic_sort(temp_suffixes,read_length*read_count);
+	bitonic_sort(temp_suffixes);
+    
+    for(int i=0;i<read_count;i++){
+        for(int j=0;j<read_length;j++)
+            cout<<temp_suffixes[i*read_length+j]<<endl;
+    }
     free(temp);
     char this_F = '$';
-    j=0;
-    
+    j=0;    
     //Calculation of F_count's
-    for(i=0;i<read_count*read_length;i++){
-        int count=0;
+    for(int i=0;i<read_count*read_length;i++){
+		int count=0;
         while(temp_suffixes[i][0]==this_F){
             count++;i++;
         }
@@ -257,7 +266,7 @@ int** makeFMIndex_student(char ***suffixes, int read_count, int read_length, int
         if(temp_suffixes[i][0]=='T')
             break;
     }
-    
+	
     //Calculation of L_student's and L_count's
     for(i=0;i<read_count*read_length;i++){
         char ch = temp_suffixes[i][read_length-1];
@@ -288,7 +297,7 @@ int** makeFMIndex_student(char ***suffixes, int read_count, int read_length, int
 //int read_length = 0;
 
 int **SA_Final;
-int **L_counts;
+
 char *L;
 int F_counts[]={0,0,0,0};
 
@@ -322,30 +331,31 @@ char** inputReads(char *file_path, int *read_count, int *length){
     *length = j+1;
     for(i=0;i<lines;i++)
         reads[i][j]='$';
-	int temp = log2((float)*length);
-	num_value = pow(2,temp);
     return reads;
 }
 //Check correctness of values
 int checker(){
     int correct = 1;
+    cout<<"cccccccccccccchecker!!!!!!!!!!"<<endl;  
     for(int i=0; i<read_count*read_length;i++){
+        
         if(L_student[i]!=L[i]){
-            //cout<<"L_student[i]!=L[i]"<<endl;
+            cout<<"L_student[i]!=L[i]"<<endl;
             correct = 0;
+            cout<<"answer is "<<L_student[i]<<endl;
         }
             
         for(int j=0;j<2;j++){
             if(SA_Final_student[i][j]!=SA_Final[i][j]){
-                //cout<<"SA_Final_student[i][j]!=SA_Final[i][j]"<<endl;
-                //cout<<SA_Final_student[i][j]<<" "<<SA_Final[i][j]<<endl;
-                correct = 0;
-            }
-                
+                cout<<"SA_Final_student[i][j]!=SA_Final[i][j]"<<endl;
+                cout<<SA_Final_student[i][j]<<" "<<SA_Final[i][j]<<endl;
+                correct = 0;  
+            }               
         }
         for(int j=0;j<4;j++){
+            //cout<<L_counts_student[i][j]<<endl;
             if(L_counts_student[i][j]!=L_counts[i][j]){
-                //cout<<"L_counts_student[i][j]!=L_counts[i][j]"<<endl;
+                cout<<"answer for"<<i<<","<<j<<" is "<<L_counts[i][j]<<endl;
                 correct = 0;
             }
                 
@@ -511,6 +521,7 @@ int main(int argc, char *argv[]){
     
     //Calculate finl FM-Index
     L_counts = makeFMIndex(suffixes, read_count, read_length, F_counts, L);
+
     
     gettimeofday(&TimeValue_Final, &TimeZone_Final);
     time_start = TimeValue_Start.tv_sec * 1000000 + TimeValue_Start.tv_usec;
@@ -528,10 +539,9 @@ int main(int argc, char *argv[]){
     for(int i=0;i<read_count;i++){
         suffixes[i]=generateSuffixes(reads[i], read_length, i);
     }
-
+        
     //Calculate finl FM-Index
-    L_counts_student = makeFMIndex_student(suffixes, read_count, read_length, F_counts_student, L_student);
-
+    L_counts_student = makeFMIndex_student(suffixes, read_count, read_length, F_counts_student, L_student);  
     //-----------Call your functions here--------------------
     gettimeofday(&TimeValue_Final, &TimeZone_Final);
     time_end = TimeValue_Final.tv_sec * 1000000 + TimeValue_Final.tv_usec;
@@ -546,13 +556,18 @@ int main(int argc, char *argv[]){
 
     //---------------Correction check and speedup calculation----------------------
     float speedup=0.0;
-    if(checker()==1)
+    fprintf(stderr,"num_value=%d\tBLOCKS=%d\tTHREADS=%d\n",num_value,BLOCKS,THREADS);
+    if(checker()==1){
+		fprintf(stderr,"congratulations!!!!\n");
         speedup = time_overhead_default/time_overhead_student;
+	}
     else
-        cout<<"X"<<endl;
+        cout<<"X but Speedup="<<time_overhead_default/time_overhead_student<<endl;
     cout<<"time_overhead_default="<<time_overhead_default<<endl;
     cout<<"time_overhead_student="<<time_overhead_student<<endl;
     cout<<"Speedup="<<speedup<<endl;
+	fprintf(stderr,"Speedup=%f\n",speedup);
     //-----------------------------------------------------------------------------
     return 0;
 }
+
